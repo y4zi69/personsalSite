b676d3f314709225ba0d40011d9c1a7c36e932fd
yazi <mohammedyasin1512@gmail,com>
2023-05-28 16:08:57 +0530

    every thing except form

--- index2.js
+++ index2.js
@@ -1,6 +1,36 @@
-function displayEducation () {
+function display (label){
+    const ed = document.querySelector('.js-ed')
+    const skill = document.querySelector('.js-section')
+    const prj = document.querySelector('.js-prj')
+    if (label === 'education'){
+        ed.classList.add("section-ed-aftr")
+        skill.classList.add('skills-section-aftr')
+        prj.classList.remove('section-prj-aftr')
 
-    const education = document.querySelector('.education')
-    education.classList.toggle('js-education')
+        
+    }else if (label === 'project'){
+        prj.classList.add('section-prj-aftr')
+        ed.classList.remove('section-ed-aftr')
+        skill.classList.add('skills-section-aftr')
 
+
+    }else if (label === 'skills'){
+        skill.classList.add('section-main')
+        skill.classList.remove('skills-section-aftr')
+        prj.classList.remove('section-prj-aftr')
+        ed.classList.remove('section-ed-aftr')
+
+    }
+
+   
+
+}
+var sideMenu = document.getElementById("side-menu");
+
+function openMenu(){
+    sideMenu.style.right = '0';
+
+}
+function closeMenu(){
+    sideMenu.style.right = '-200px';
 }
